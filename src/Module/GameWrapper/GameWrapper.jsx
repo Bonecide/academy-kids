3614389a0c805977ef951ed506303d2177fd55ee
Kasymov Marsel <marselkasymov23@gmail.com>
2022-08-08 20:04:49 +0600

    added adaptive on games

--- src/Module/GameWrapper/GameWrapper.jsx
+++ src/Module/GameWrapper/GameWrapper.jsx
@@ -3,6 +3,7 @@ import ChooseTemp from '../../Components/chooseTemp/ChooseTemp'
 import Select from '../../Components/selects/Select'
 import s from './GameWrapperAnzan.module.scss'
 import Settings from './../../Components/Settings/Settings';
+import './timly.scss'
 
 export default function GameWrapper({title ,img}) {
     const [isSettings,setIsSettings] = useState(false)
@@ -37,12 +38,25 @@ export default function GameWrapper({title ,img}) {
                 
                 {!isSettings ? (
                 <>
+                    <div className={s.mobileOnly}>
+                        <div>
+                                <h1>{title}</h1>
+                        </div>
+                    <div className={s.gameSettings__container}>
+                        <div className={s.gameSettings}>
+                                    <img src={'/img/icons/play.svg'} alt="" />
+                            </div>
+                            <div onClick={() => setIsSettings(!isSettings)} style= {{'background' : isSettings && 'linear-gradient(142.82deg, #97CE4E 11.11%, #58AA2C 83.28%)'}} className={s.gameSettings}>
+                                    <img src={'/img/icons/gamesetting.svg'} alt="" />
+                            </div>
+                        </div>
+                    </div>
                     <div className={s.settings}>
-                    <Select/>
-                    <ChooseTemp/>
-                    <ChooseTemp/>
-                    <ChooseTemp title='Время, сек.' type="Скорость"/>
-                </div>
+                    <Select     className={'inputs'}/>
+                    <ChooseTemp className={'inputs'}/>
+                    <ChooseTemp className={'inputs'}/>
+                    <ChooseTemp className={'inputs'} title='Время, сек.' type="Скорость"/>
+                    </div>
                 <div className={s.button}>
                     <button>Начать</button>
                 </div>
