76f844a8d1b1adc62ddbf9b71fecf4e09679ad8d
Kasymov Marsel <marselkasymov23@gmail.com>
2022-08-11 14:06:23 +0600

    added adaptive on Personal Account

--- src/Components/PersonalWrap/PersonalWrap.jsx
+++ src/Components/PersonalWrap/PersonalWrap.jsx
@@ -12,6 +12,22 @@ export default function PersonalWrap() {
             <div className={s.parent}>
                 <div className={s.absolute}>
                     <img src="/img/AccountAvatar.svg" alt="" />
+                    <div className={s.choose}>
+                        <div className={s.arrowbox}>
+                            <img src="/img/icons/arrow-back.svg" alt="" />
+                        </div>
+                        <div className={s.bluebuttons}>
+                            <div className={`${s.box} ${s.stat}`}>
+                                <p>Статистика</p>
+                            </div>
+                            <div className={s.box} >
+                                <p>Задания</p>
+                            </div>
+                        </div>
+                        <div className={s.arrowbox}>
+                            <img src="/img/icons/delete.svg" alt="" />
+                        </div>
+                    </div>
                 </div>
                 <div className={s.wrap}>
                     <MainInfoAcc/>
